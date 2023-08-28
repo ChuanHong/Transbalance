66a30eb0223b579a3bdeb5462b94103673706495
Chuan <hong.chuan.hannah@gmail.com>
2023-08-27 20:46:25 -0400

    update

--- R/Coef.gen.R
+++
@@ -1,7 +0,0 @@
-Coef.gen<- function(s, h, sig.beta, sig.delta, p){
-  beta0 <- c(rep(sig.beta,s), rep(0, p - s))
-  W <- beta0
-  samp0<- sample(1:s, h, replace=F)
-  W[samp0] <-W[samp0] + rep(-sig.delta, h)
-  return(list(W=W, beta0=beta0))
-}
--- R/val.fun.new.R
+++
@@ -1,5 +0,0 @@
-val.fun.new=function(mybeta, dat.val,nm.out){
-  tmp.x=dat.val[,names(mybeta)]
-  mys=data.matrix(tmp.x)%*%as.numeric(mybeta)
-  list(auc=ROC.Est.FUN(dat.val[,nm.out], mys, yy0=0.5)[1],S=mys)
-}
