b2d282f092a008155f812502ca27b7a0a293ee1e
Chuan <hong.chuan.hannah@gmail.com>
2023-08-27 20:43:28 -0400

    update

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,12 @@
+#' @export
 g.logit = function(xx){exp(xx)/(exp(xx)+1)}
+#' @export
 logit = function(xx){log(xx/(1-xx))}
+#' @export
 dg.logit = function(xx){exp(xx)/(exp(xx)+1)^2}
+#' @export
 Intercept.GLM = function(yi,bxi){glm(yi~bxi,family="binomial")}
-
+#' @export
 ROC.FUN.Sup.Par = function(St,Yt,fpr=jump.u){
   mhati = g.logit(St); ss = unique(sort(St)); mu1 = mean(Yt); mu0 = 1-mu1; nt = length(Yt)
   S1.ss = sum.I(ss, "<=", St, mhati)/sum(mhati)
@@ -13,7 +17,7 @@ ROC.FUN.Sup.Par = function(St,Yt,fpr=jump.u){
   list(auc,out)
 }
 
-
+#' @export
 ROC.FUN.SSL.NP = function(Sv,St,Yt,fpr=jump.u,yes.CV=F,Xt=NULL,Xv=NULL,rep=10,regularize=yes.regularize){
   nv = length(Sv); nt = length(Yt); Pt = sum.I(St, ">=", Sv)/nv
   Pv = sum.I(Sv,">=",Sv)/nv; bw = sd(Pt)/nt^0.3; ss = sort(Pv)
@@ -42,7 +46,7 @@ ROC.FUN.SSL.NP = function(Sv,St,Yt,fpr=jump.u,yes.CV=F,Xt=NULL,Xv=NULL,rep=10,re
   out=sapply(1:ncol(out),function(kk){approx(out[,"FPR"],out[,kk],fpr,rule=2)$y}); colnames(out)=tmpnm
   list(auc,out)
 }
-
+#' @export
 ROC.FUN.SSL.Par = function(Xv,dat.t,fpr=jump.u,yes.CV=F,rep=10,wgt=NULL,rtn="list",bptb=NULL,bptb2=NULL,regularize=yes.regularize){
   if(is.null(wgt)){wgt=rep(1,nrow(dat.t))}
   if(is.null(bptb)){bptb = Est.ALASSO.GLM(dat.t,rtn="EST",regularize=regularize,Wi=wgt)[1+0:ncol(Xv)]}
@@ -79,6 +83,7 @@ ROC.FUN.SSL.Par = function(Xv,dat.t,fpr=jump.u,yes.CV=F,rep=10,wgt=NULL,rtn="lis
 ## data: 1st column y; remaining x; nopen.ind indexes which subset of x should not be penalized                       ##
 ## Wi: weights for resampling if interested in obtaining standard errors                                              ## 
 ## ================================================================================================================== ##
+#' @export
 Est.ALASSO.GLM = function(data,Wi=NULL,rtn="EST",nopen.ind=NULL,regularize=yes.regularize){
   data = as.matrix(data); y = data[,1]; x = data[,-1,drop=F]; nn=length(y); if(is.null(Wi)){Wi=rep(1,nn)}; pp = ncol(x)
   if(regularize){
@@ -110,7 +115,7 @@ Est.ALASSO.GLM = function(data,Wi=NULL,rtn="EST",nopen.ind=NULL,regularize=yes.r
   out = c("b"=bhat, "bini"=bini,"lamhat"=lamhat,"lam.all"=lam.all,"BIC.lam"=BIC.lam,"b.all"=b.all)
   if(rtn=="EST"){return(out)}else{return(list(out,"b.all"=b.all,"lam.all"=lam.all,"fit"=tmpfit,"BIC.lam"=BIC.lam))}
 }
-
+#' @export
 SIM.FUN = function(nn,rtn="data.t")
   {
 	xx = mvrnorm(nn,mu=rep(0,p.x),Sigma=Sig0.X); 
@@ -124,7 +129,7 @@ SIM.FUN = function(nn,rtn="data.t")
   }
 
 
-
+#' @export
 ROC.FUN.ALASSO.632boot  <- function(data, ind=NULL, wgti=NULL, Wi=NULL, yy0 = 0.5, nopen.ind=NULL,
                             FPR0=seq(.01,.99,by=.01),rtn="ALL",yes.CV=F,yes.seed=F,rep=10,regularize=T){
   ## ========================================================================= ##
@@ -162,7 +167,7 @@ ROC.FUN.ALASSO.632boot  <- function(data, ind=NULL, wgti=NULL, Wi=NULL, yy0 = 0.
      return(out)
    }else{return(list("rochat"=rochat, "roc.cv"=roc.cv, "beta"=betahat, "Si"=yyi, "yyi.v"=yyi.vmat))}
 }
-
+#' @export
 ROC.FUN.ALASSO.cv  <- function(ind=NULL,data, wgti=NULL, Wi=NULL, yy0 = 0.5, nopen.ind=NULL,
                             FPR0=seq(.01,.99,by=.01),rtn="ALL",yes.CV=F,yes.seed=F,rep=10,regularize=T,lam.ridge=0)
   {
@@ -228,7 +233,7 @@ ROC.FUN.ALASSO.cv  <- function(ind=NULL,data, wgti=NULL, Wi=NULL, yy0 = 0.5, nop
      return(out)
    }else{return(list(rochat, roc.cv, betahat, yyi))}
   }
-
+#' @export
 ROC.Est.FUN <- function(Di,yyi,yy0,fpr0=NULL,wgti=NULL,yes.smooth=F)
   {
     out.yy <- out.pp <- out.AUC <- out.TPR <- out.FPR <- out.PPV <- out.NPV <- NULL
@@ -261,14 +266,14 @@ ROC.Est.FUN <- function(Di,yyi,yy0,fpr0=NULL,wgti=NULL,yes.smooth=F)
     out = c(out.AUC,out.yy,out.pp,out.FPR,out.TPR,out.PPV,out.NPV)
     out
   }
-
+#' @export
 AUC.FUN = function(data)
   {
 	dd = data[,1]; xx = data[,2]; n0 = sum(1-dd); n1 = sum(dd) 
 	x0 = xx[dd==0]; x1 = xx[dd==1]
   	sum((sum.I(x0, "<=", x1)+sum.I(x0,"<",x1))/2)/(n0*n1)
   }
-
+#' @export
 chat.FUN.632boot <- function(data,fpr0=0.05,betahat,regularize=T,nopen.ind=NULL)
   {
     yy = data[,1]; xx = as.matrix(data[,-1]); p0 = ncol(data); data = as.matrix(data); nn = length(yy)
@@ -289,7 +294,7 @@ chat.FUN.632boot <- function(data,fpr0=0.05,betahat,regularize=T,nopen.ind=NULL)
     c.fpr.bc  = 0.368*c.fpr.0 + 0.632*apply(as.matrix(c.all),1,mean,na.rm=T)     
     c(g.logit(c.fpr.0),g.logit(c.fpr.bc))
   }
-
+#' @export
 chat.FUN <- function(data,fpr0=0.05,betahat,regularize=T,nopen.ind=NULL)
   {
     yy = data[,1]; xx = as.matrix(data[,-1]); p0 = ncol(data)
@@ -316,7 +321,7 @@ chat.FUN <- function(data,fpr0=0.05,betahat,regularize=T,nopen.ind=NULL)
     c.fpr.bc  = c.fpr.0 + apply(c.all-c.t.all,2,mean,trim=0.05)
     g.logit(c(c.fpr.0,c.fpr.bc))
   }
-
+#' @export
 Predict.FUN <- function(data,newdata,fpr0=0.05,betahat)
   {
     yy = data[,1]; xx = as.matrix(data[,-1]); p0 = ncol(data)
@@ -346,28 +351,29 @@ Predict.FUN <- function(data,newdata,fpr0=0.05,betahat)
     data.frame("patient_num"=newdata$patient,"Prob.RA"=round(pnew.0,5),
     "Cut-off.0"=round(c.fpr.0,5), "Cut-off.bc"=round(c.fpr.bc,5))
   }
-
+#' @export
 ProbTestPos <- function(tpr,fpr,prev)
   {
     tpr*prev + fpr*(1-prev)
   }
       
-    
+#' @export    
 L2Norm <- function(data,coef,intercept=F)
   {
     yy = data[,1]; xx = data[,-1]; if(intercept){xx=cbind(1,xx)}
     apply((yy - xx%*%t(coef))^2,2,mean)
   }
 
-                        
+#' @export                        
 PPV.FUN <- function(fpr,SE, mu0){ 1/(1+fpr/SE*(1-mu0)/mu0)}
+#' @export
 NPV.Project <- function(npv.e0y0,p.e1,p.y0.e0,npv.e1=1)
   {
   	## P(D=0 | E=1 or E=0&Y=0) = {P(D=0|E=1)P(E=1)+P(D=0|E=0&Y=0)P(E=0&Y=0)}/{P(E=1)+P(E=0&Y=0)
   	npv.all = (npv.e1 * p.e1 + npv.e0y0*p.y0.e0*(1-p.e1))/(p.e1+p.y0.e0*(1-p.e1))
   	npv.all
   }
-
+#' @export
 S.FUN <- function(yy,Yi,Di,yes.smooth=F)
   {
   	if(yes.smooth){
@@ -378,15 +384,14 @@ S.FUN <- function(yy,Yi,Di,yes.smooth=F)
   	}
     ##sum.I(yy,"<=",Yi,Vi=Di)/sum(Di)
   }
-
+#' @export
 Sinv.FUN <- function(uu,Yi,Di,yes.smooth=F)
   {
     yy0<-unique(sort(Yi,decreasing=T)); ss0 <- S.FUN(yy0,Yi,Di,yes.smooth=yes.smooth) 
     return(approx(ss0[!duplicated(ss0)],yy0[!duplicated(ss0)],uu,method="linear",f=0,rule=2)$y)
   }
 
-
-
+#' @export
 CV.FUN <- function(data)
   {
     y = data[,1]; x = data[,-1]; nn=length(y)
@@ -406,11 +411,11 @@ CV.FUN <- function(data)
     bhat  = l1ce(y~x.w, standardize=F, bound=s0)
     list("b"=coef(bhat)/w, "s0"=s0, "lam0" =bhat$Lagrangian,"b0"=bhat$bound)    
   }
-
+#' @export
 VTM<-function(vc, dm){
     matrix(vc, ncol=length(vc), nrow=dm, byrow=T)
 }
-
+#' @export
 sum.I <- function(yy,FUN,Yi,Vi=NULL)
 ## sum_i I(yy FUN Yi)Vi
 # Vi weight
@@ -426,7 +431,8 @@ sum.I <- function(yy,FUN,Yi,Vi=NULL)
         Vi <- apply(as.matrix(Vi)[tmpind,,drop=F],2,cumsum)
         return(rbind(0,Vi)[pos+1,])
     } else return(pos)
-  }
+}
+#' @export
 convert <- function(fit) {
 	rochat.auc = fit$rochat[1]
 	rochat.values = matrix(fit$rochat[-1],ncol=6)
@@ -438,13 +444,13 @@ convert <- function(fit) {
 	names(betahat) = gsub("\\<b\\.","",names(betahat))
 	return(list(ROC.hat.auc=rochat.auc, ROC.hat.values=rochat.values, ROC.cv.auc=roc.cv.auc,ROC.cv.values=roc.cv.values, beta=betahat, Y.hat=fit$Si))
 }
-
+#' @export
 AUC = function(D,score,wgt=NULL){
 	if(is.null(wgt)) {wgt=rep(1,length(D))}
 	auc = sum(S.FUN(score,Yi=score,D*wgt,yes.smooth=F)*(1-D)*wgt)/sum((1-D)*wgt)
 	return(auc)
 }
-
+#' @export
 ROC = function(D,score){
 	roc = ROC.Est.FUN(D,score,0.5,seq(.01,.99,by=.01))
 	roc = matrix(roc[-1],ncol=6)
@@ -453,14 +459,14 @@ ROC = function(D,score){
 }
 
 
-###
+#' @export
 intercept.fun=function(n, p, beta0, mean.X, Sig.X, prev){
   x <- rmvnorm(n, rep(mean.X, p), Sig.X)
   beta0(betaX=beta0, X=cbind(1,x), N=rep(1,dim(x)[1]), rhoY=prev, expandX="all")
   
   y=unlist(lapply(1:n, function(ll) rbinom(1,1,g.logit(c(1,x[ll,]) %*% beta0))))
 }
-
+#' @export
 Data.gen.sim=function(M, n.s, n.t,n.t.other, n.v, p, beta0.s, beta0.t, mean.X.s, mean.X.t, Sig.X.s, Sig.X.t, inter){
   dat.s.list=dat.t.list=vector("list", length = M)
   dat.v=Data.gen2(n.v, p, beta0.t, mean.X.t, Sig.X.t,inter)
@@ -478,7 +484,7 @@ Data.gen.sim=function(M, n.s, n.t,n.t.other, n.v, p, beta0.s, beta0.t, mean.X.s,
   
   return(list(dat.v=dat.v, dat.t.list=dat.t.list, dat.s.list=dat.s.list))
 }
-
+#' @export
 Data.gen1=function(n, p, prev, mean.X1, mean.X0, Sig.X){
   y=rbinom(n,1,prev)
   x=matrix(NA, ncol=p, nrow=n)
@@ -487,7 +493,7 @@ Data.gen1=function(n, p, prev, mean.X1, mean.X0, Sig.X){
   data.frame(y, x)
 }
 
-
+#' @export
 Coef.gen<- function(s, h, sig.beta, sig.delta, p){
   beta0 <- c(rep(sig.beta,s), rep(0, p - s))
   W <- beta0
@@ -495,7 +501,7 @@ Coef.gen<- function(s, h, sig.beta, sig.delta, p){
   W[samp0] <-W[samp0] + rep(-sig.delta, h)
   return(list(W=W, beta0=beta0))
 }
-
+#' @export
 Coef.gen.new<- function(s, h, sig.beta, sig.delta, p){
   beta0 <- c(rep(sig.beta,s), rep(0, p - s))
   W <- beta0
@@ -503,7 +509,7 @@ Coef.gen.new<- function(s, h, sig.beta, sig.delta, p){
   W[samp0] <-W[samp0] + rep(sig.delta, h)
   return(list(W=W, beta0=beta0))
 }
-
+#' @export
 table.sim.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=5000, n.t.list, prev.t.list, setting.list, 
                        sampling.list, model.list, fed.list, by.cols=c("Model")){
   cols.rm=unique(c(setdiff(c("M","n.s", "n.t", "prev.t","p","s", "sig.beta", "setting", "Sampling", "Model", "Fed", "FRM"),by.cols),
@@ -524,7 +530,7 @@ table.sim.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=5000, n.t.li
   mytable.new
 }
 
-
+#' @export
 table.sim.FindBest.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=5000, n.t.list, prev.t.list, setting.list, 
                                 sampling.list, model.list, fed.list, by.cols=c("Model")){
   cols.rm=unique(c(setdiff(c("M","n.s", "n.t", "prev.t","p","s", "sig.beta", "setting", "Sampling", "Model", "Fed", "FRM"),by.cols),
@@ -545,7 +551,7 @@ table.sim.FindBest.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=500
   mytable.new[which.max(mytable.new$auc.est),]
 }
 
-
+#' @export
 table.sim.print.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=5000, n.t.list, prev.t.list, setting.list, 
                              sampling.list, model.list, fed.list, by.cols=c("Model")){
   cols.rm=unique(c(setdiff(c("M","n.s", "n.t", "prev.t","p","s", "sig.beta", "setting", "Sampling", "Model", "Fed", "FRM"),by.cols),
@@ -565,7 +571,7 @@ table.sim.print.fun=function(mytable, ave.method=mean, M.list=4, n.s.list=5000, 
   mytable.new=mytable.new[ do.call(order, as.list(as.data.frame(mytable.new[,by.cols])) ), ]
   mytable.new
 }
-
+#' @export
 table.data.print.fun=function(mytable, ave.method=mean, nm.study.list, year.cut.list, sampling.list, model.list, fed.list, frm.list, by.cols=c("Cohort","Model")){
   cols.rm=unique(c(setdiff(c("Cohort", "Year", "Sampling", "Model", "Fed", "FRM"),by.cols),
                    "fpr", "cut", "p.pos", "npv"))
@@ -582,7 +588,7 @@ table.data.print.fun=function(mytable, ave.method=mean, nm.study.list, year.cut.
   mytable.new=mytable.new[ do.call(order, as.list(as.data.frame(mytable.new[,by.cols])) ), ]
   mytable.new
 }
-
+#' @export
 table.fun=function(mytable, ave.method=mean, nm.study.list, year.cut.list, sampling.list, model.list, fed.list, frm.list, by.cols=c("Cohort","Model")){
   cols.rm=unique(c(setdiff(c("Cohort", "Year", "Sampling", "Model", "Fed", "FRM"),by.cols),
                    "fpr", "cut", "p.pos", "npv"))
@@ -600,15 +606,15 @@ table.fun=function(mytable, ave.method=mean, nm.study.list, year.cut.list, sampl
   mytable.new
 }
 
-
+#' @export
 myrose.fun=function(dat,N.rose=5000, myseed){
   ROSE(Y~., data=dat, seed = myseed, N=max(N.rose, max(table(dat[,"Y"]))*2))$data
 }
-
+#' @export
 myrose.fed.fun=function(dat,N.rose=5000, myseed){
   ROSE(Y~., data=dat, seed = myseed, N=max(N.rose, max(table(dat[,"Y"]))*2))$data
 }
-
+#' @export
 myrose.list.fun=function(dat.train, dat.list, nm.study, N.rose=5000, myseed){
   dat.list.new=NULL
   for(ll in setdiff(ls(dat.list), nm.study)){
@@ -618,18 +624,19 @@ myrose.list.fun=function(dat.train, dat.list, nm.study, N.rose=5000, myseed){
   dat.list.new[[nm.study]]=data.matrix(dat.train)
   dat.list.new
 }
-
+#' @export
 val.fun.new=function(mybeta, dat.val,nm.out){
   tmp.x=dat.val[,names(mybeta)]
   mys=data.matrix(tmp.x)%*%as.numeric(mybeta)
   list(auc=ROC.Est.FUN(dat.val[,nm.out], mys, yy0=0.5)[1],S=mys)
 }
+#' @export
 val.fun=function(mybeta, dat.val){
   tmp.x=dat.val[,names(mybeta)]
   mys=data.matrix(tmp.x)%*%as.numeric(mybeta)
   list(auc=ROC.Est.FUN(dat.val$stroke, mys, yy0=0.5)[1],S=mys)
 }
-
+#' @export
 myroc.fun=function(junk, cut.nm="fpr", cut.val=0.1){
   mt=junk[-1]
   mt=data.frame(matrix(mt, ncol=6, byrow=F))
@@ -638,7 +645,7 @@ myroc.fun=function(junk, cut.nm="fpr", cut.val=0.1){
   indx=which.min(abs(mt[,cut.nm]-cut.val))
   mt[indx,]
 }
-
+#' @export
 Trans.update.fun=function(beta.ini, x.t, y.t, family="binomial"){
   p=dim(x.t)[2]
   n.t=length(y.t)
@@ -649,7 +656,7 @@ Trans.update.fun=function(beta.ini, x.t, y.t, family="binomial"){
   beta.fit <- beta.ini + delta.fit
   beta.fit
 }
-
+#' @export
 Trans.fun=function(x.s, y.s, x.t, y.t, family="binomial"){
   n.s=length(y.s)
   n.t=length(y.t)
@@ -664,7 +671,7 @@ Trans.fun=function(x.s, y.s, x.t, y.t, family="binomial"){
   beta.fit <- w.fit + delta.fit
   beta.fit
 }
-###
+#' @export
 iteration.fun2=function (dat.list, bini, kk.list) {
   K = length(dat.list)
   Uini.list = sapply(kk.list, function(kk) {
@@ -677,6 +684,7 @@ iteration.fun2=function (dat.list, bini, kk.list) {
   bhat.list = -ginv(Ahat.ini) %*% Uini.list + bini
   list(b.k = bhat.list, Ahat = Ahat.ini)
 }
+#' @export
 DCOS.FUN=function(dat.list, niter, ridge=F, lambda=NULL){
   K=length(dat.list)
   lambda.grid=10^seq(-100,3,0.1)
@@ -706,7 +714,7 @@ DCOS.FUN=function(dat.list, niter, ridge=F, lambda=NULL){
   betahat = Est.ALASSO.Approx.GLMNET(ynew=Ahalf.DCOS%*%bnew.update,xnew=Ahalf.DCOS,bini=bnew.update,N.adj=N,lambda.grid)$bhat.BIC
   betahat
 }
-
+#' @export
 DCOS.Trans.FUN=function(bini, dat.list){
   K=length(dat.list)
   lambda.grid=10^seq(-100,3,0.1)
@@ -721,7 +729,7 @@ DCOS.Trans.FUN=function(bini, dat.list){
   betahat
 }
 
-####
+#' @export
 Trans_DCOS.FUN=function(dat.list, niter, ridge=F, lambda=NULL){
   K=length(dat.list)
   lambda.grid=10^seq(-100,3,0.1)
@@ -752,7 +760,7 @@ Trans_DCOS.FUN=function(dat.list, niter, ridge=F, lambda=NULL){
   betahat
 }
 
-
+#' @export
 density_fit_fun=function(y.s, x.s, y.t, x.t, is.pca, fit.type="all"){
   y.dr.fit=c(rep(0,length(y.s)), rep(1,length(y.t)))
   x.dr.fit=rbind(x.s, x.t)
@@ -769,7 +777,7 @@ density_fit_fun=function(y.s, x.s, y.t, x.t, is.pca, fit.type="all"){
   fit.ds=Est.ALASSO.GLMNET(data=cbind(y.fit,x.fit),fam0="binomial", Wi=pp1/(1-pp1))$bhat.BIC
   fit.ds
 }
-
+#' @export
 density_fed_fit_fun=function(y.s.list, x.s.list, y.t.list, x.t.list, y.s, x.s, y.t, x.t, is.pca){
   y.dr.fit.list=lapply(1:length(y.s.list),function(ll) c(rep(0,length(y.s.list[[ll]])), rep(1,length(y.t.list[[ll]]))))
   x.dr.fit.list=lapply(1:length(x.s.list),function(ll) rbind(x.s.list[[ll]], x.t.list[[ll]]))
@@ -790,8 +798,7 @@ density_fed_fit_fun=function(y.s.list, x.s.list, y.t.list, x.t.list, y.s, x.s, y
   fit.ds=Est.ALASSO.GLMNET(data=cbind(y.fit,x.fit),fam0="binomial", Wi=pp1/(1-pp1))$bhat.BIC
   fit.ds
 }
-
-
+#' @export
 cohort.sel.fun=function(dat.split, nm.cohort.t, nm.cov,rr=2){ 
   x.t=dat.split[dat.split$cohort==nm.cohort.t ,nm.cov]
   y.t=dat.split[dat.split$cohort==nm.cohort.t,"stroke"]
@@ -814,7 +821,7 @@ cohort.sel.fun=function(dat.split, nm.cohort.t, nm.cov,rr=2){
   beta.sel=beta.sel[1:5]
   beta.sel
 }
-
+#' @export
 evaluate.fun=function(y,s, cut.metric="fpr", metric.cut=0.5){
   junk=ROC.Est.FUN(y,s, yy0=0.5,fpr0=seq(0,1,0.01))
   junk.mtx=data.frame(matrix(junk[-1], ncol=6))
@@ -832,6 +839,7 @@ evaluate.fun=function(y,s, cut.metric="fpr", metric.cut=0.5){
   colnames(myroc)=c("cut.metric", "metric.cut","auprc", "aucroc", colnames(junk.mtx), "f1.max")
   myroc
 }
+#' @export
 lasso.fun=function(x,y, family, offset=NULL, weights=NULL, penalty.factor=NULL, lambda=NULL,alpha=1){
   if(is.null(weights)){weights=rep(1,dim(x)[1])}
   if(is.null(penalty.factor)){penalty.factor=rep(1,dim(x)[1])}
@@ -845,7 +853,7 @@ lasso.fun=function(x,y, family, offset=NULL, weights=NULL, penalty.factor=NULL, 
   }
   as.numeric(coef.1se)
 }
-
+#' @export
 weight.fun=function(x.source, y.source, x.target, y.target){
   #y.tmp=c(rep(0,length(y.target)), rep(1, length(y.target)))
   y.tmp=c(rep(1,length(y.source)), rep(0, length(y.target)))
@@ -858,7 +866,7 @@ weight.fun=function(x.source, y.source, x.target, y.target){
   auc=ROC.Est.FUN(y.tmp,p.all,yy0=0.5)[1]
   list(beta=junk,auc=auc,p.all=p.all, p.source=p.source, p.target=p.target)
 }
-
+#' @export
 data.gen.fun=function(n.source, n.target, p, setting,
                       setting1.delta.mean=1,
                       setting1.delta.sd=1,
@@ -909,7 +917,7 @@ data.gen.fun=function(n.source, n.target, p, setting,
   return(list(b.source=b.source, b.target=b.target,y.source=y.source, y.target=y.target, x.source=x.source, x.target=x.target))
 }
 
-
+#' @export
 data.gen.fun2=function(n.s, n.t, p, a, sigma2.ee){
   x.s=matrix(rnorm(n.s*p, 0, 1),ncol=p)
   x.t=matrix(rnorm(n.t*p, 0, 1),ncol=p)
@@ -921,7 +929,7 @@ data.gen.fun2=function(n.s, n.t, p, a, sigma2.ee){
   y.t=unlist(lapply(g.logit(cbind(1,x.t)%*%theta.t), function(pp) rbinom(1,1,pp)))
   return(list(y.s=y.s, x.s=x.s, y.t=y.t, x.t=x.t, theta.s=theta.s, theta.w=theta.w, theta.t=theta.t))
 }
-
+#' @export
 lasso.fun2=function(x,y, family, offset=NULL, weights=NULL, lambda=NULL,alpha=1, method="lasso"){
   if(is.null(weights)){weights=rep(1,dim(x)[1])}
   if(method=="lasso"){
@@ -940,6 +948,7 @@ lasso.fun2=function(x,y, family, offset=NULL, weights=NULL, lambda=NULL,alpha=1,
   }
   res
 }
+#' @export
 Est.ALASSO.GLMNET=function (data, BIC.factor = 0.1, fam0 = "binomial", w.b = NULL, Wi=NULL, lambda.grid = 10^seq(-4,0,0.01)) 
 {  if(is.null(Wi)==1){Wi=rep(1,dim(data)[1])}
   if (fam0 != "Cox") {
@@ -1000,7 +1009,7 @@ Est.ALASSO.GLMNET=function (data, BIC.factor = 0.1, fam0 = "binomial", w.b = NUL
   return(list(bhat.BIC = bhat.BIC, bhat.modBIC = bhat.modBIC, bhat.AIC=bhat.AIC,
               lambda.BIC = lamhat.BIC, lambda.modBIC = lamhat.modBIC, lambda.AIC=lamhat.AIC))
 }
-
+#' @export
 Est.ALASSO.GLMNET.offset=function (data, BIC.factor = 0.1, fam0 = "binomial", w.b = NULL, Wi=NULL, lambda.grid = 10^seq(-4,0,0.01), myoffset) 
 {  if(is.null(Wi)==1){Wi=rep(1,dim(data)[1])}
   if (fam0 != "Cox") {
